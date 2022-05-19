a63f4b765dc8db35481e4f45f0ffa253cd34b775
gxstar <gengxing123@qq.com>
2022-05-19 23:25:05 +0800

    解决ts判断字典为空

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,8 +5,8 @@ let articleList=ref({});
 
 function add_a() {
     console.log('点了');
-    console.log(articleList.value);
-    // articleList.value={'id':1,'title':'test标题'};
+    console.log(articleList);
+    articleList.value={'id':1,'title':'test标题'};
     // console.log(articleList.value['title']);
 }
 
@@ -14,12 +14,12 @@ function add_a() {
 
 <template>
 <h4>最新文章</h4>
-<section v-if="articleList.value=={}">
-    <el-empty description="暂时没有文章" />
+<section v-if="Object.keys(articleList).length!=0">
+    有货
     <el-button @click="add_a">点击添加内容</el-button>
 </section>
 <section v-else>
-    有货
+    <el-empty description="暂时没有文章" />
     <el-button @click="add_a">点击添加内容</el-button>
 </section>
 </template>
